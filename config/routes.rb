bb0f624701cfe73ca1690542f855cc71c37cbdf2
harujamu <tnagisa1220@gmail.com>
2022-06-30 02:57:34 +0000

    [Finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for:users
   root to:'homes#top'
   get 'homes/about' => 'homes#about', as:'about'
-  resources:users,only:[:show,:edit]
+  resources:users,only:[:show,:edit, :update]
   resources:post_images,only:[:new, :index, :show, :create, :destroy]
   
   
